ad70c9132fbbc0221213a29b34fe2db538e14ac8
Sergio Lecuona <serlecu@hotmail.com>
2023-04-04 14:59:57 +0200

    dirty commit
    - not finding a advertise method in SimplePyBLE

--- node_display/src/bluetooth.py
+++ node_display/src/bluetooth.py
@@ -2,26 +2,6 @@ import simplepyble as ble
 import time
 import src.globals as g
 
-def setupBTAdapter():
-  print("Initializing Bluetooth...")
-  print(f"Running on {ble.get_operating_system()}")
-
-  isAdapterSet = False
-  while not isAdapterSet:
-    adapters = ble.Adapter.get_adapters()
-    if len(adapters) == 0:
-          print("No adapters found")
-          time.sleep(2)
-    else:
-      for adapter in adapters:
-          print(f"Adapter: {adapter.identifier()} [{adapter.address()}]")
-
-      g.BTAdapter = adapters[0] # I take the first adapter IDKW
-      g.BTAdapter.set_callback_on_scan_start(lambda: print("Scan started."))
-      g.BTAdapter.set_callback_on_scan_stop(lambda: print("Scan complete."))
-      g.BTAdapter.set_callback_on_scan_found(lambda peripheral: print(f"Found {peripheral.identifier()} [{peripheral.address()}]"))
-      isAdapterSet = True
-
 
 def scanBT():
     g.isScanning = True
@@ -41,6 +21,12 @@ def filter_devices(devices, name):
 def connectBT(device):
     print("Connecting to {}".format(device.address()))
     device.connect()
+    print("Successfully connected, listing services...")
+    services = device.services()
+    service_characteristic_pair = []
+    for service in services:
+        for characteristic in service.characteristics():
+            service_characteristic_pair.append((service.uuid(), characteristic.uuid()))
 
 def sendBT(sock, data):
     print("Sending {}".format(data))
@@ -52,7 +38,46 @@ def receiveBT(sock):
     # print("Received {}".format(data))
     # return data
 
+def onDeviceFound(device):
+    customUUID = "4A98xxxx-1CC4-E7C1-C757-F1267DD021E8"
+    print(f"Found: {device.identifier()} [{device.address()}]")
+    if device.is_connectable():
+        services = device.services()
+        for service in services:
+            for characteristic in service.characteristics():
+                if service.uuid() == customUUID:
+                    print("Found GOLEM service")
+                    connectBT(device)
+                    break
 
+def onDeviceUpdated(device):
+    print(f"Updated: {device.identifier()} [{device.address()}]")
+
+def setupBTAdapter():
+  print("Initializing Bluetooth...")
+  print(f"Running on {ble.get_operating_system()}")
+
+  isAdapterSet = False
+  while not isAdapterSet:
+    adapters = ble.Adapter.get_adapters()
+    if len(adapters) == 0:
+          print("No adapters found")
+          time.sleep(2)
+    else:
+      for adapter in adapters:
+          print(f"Adapter: {adapter.identifier()} [{adapter.address()}]")
+
+      g.BTAdapter = adapters[0] # I take the first adapter IDKW
+      #print(g.BTAdapter.identifier())
+      g.BTAdapter.set_callback_on_scan_start(lambda: print("Scan started."))
+      g.BTAdapter.set_callback_on_scan_stop(lambda: print("Scan complete."))
+      g.BTAdapter.set_callback_on_scan_found(lambda peripheral: onDeviceFound(peripheral))
+      g.BTAdapter.set_callback_on_scan_updated(lambda peripheral: onDeviceUpdated(peripheral))
+      isAdapterSet = True
+
+  BTService = ble.Service()
+  BTService.uuid = "4A98XXXX-1CC4-E7C1-C757-F1267DD021E8"
+  g.BTAdapter.add_service(BTService)
 
 def handleBTConnections():
     pass
