c2450ef10eaa2441fb643c8c4d5a7e24973142d3
Sergio Lecuona <serlecu@hotmail.com>
2023-04-07 16:58:38 +0200

    Successful connect to vaina-sensor
    - fix crash on switch-off vaina-sensor
    - fix connection issues

--- node_vaina-visualizer/src/bluetooth.py
+++ node_vaina-visualizer/src/bluetooth.py
@@ -5,6 +5,7 @@ import simplepyble as ble
 import src.globals as g
 
 BTAdapter: ble.Adapter #type: ignore
+devicesChecked = False
 
 def setupBTAdapter():
   global BTAdapter
@@ -21,55 +22,90 @@ def setupBTAdapter():
           print(f"Adapter: {adapter.identifier()} [{adapter.address()}]")
 
       BTAdapter = adapters[0] # I take the first adapter IDKW                   
-      BTAdapter.set_callback_on_scan_start(lambda: print("Scan started."))      
-      #g.BTAdapter.set_callback_on_scan_stop(lambda: print("Scan complete."))
-      BTAdapter.set_callback_on_scan_found(lambda peripheral: filter_device(peripheral, g.targetUUID) )
+      BTAdapter.set_callback_on_scan_start( lambda: onScanStart() )      
+      BTAdapter.set_callback_on_scan_stop( lambda: onScanStop() )
+      BTAdapter.set_callback_on_scan_found(lambda peripheral: filterDevice(peripheral, g.targetUUID) )
       isAdapterSet = True
-
 
 
 def scanBT():
     global BTAdapter
-    g.isScanning = True
     BTAdapter.scan_for(4000)                        
-    g.foundDevices = BTAdapter.scan_get_results()   #type: ignore
-    g.isScanning = False
-    print("Scan complete")
+    # g.foundDevices = BTAdapter.scan_get_results()   #type: ignore
 
-def filter_device(device, targetUUID):
-    
-    # Check if can connect
-    if (device.is_connectable()):
-        print(f"Found {device.identifier()} [{device.address()}]")
-        services = device.services()
-        for service in services:
-            if service.uuid() == targetUUID:
-                print(f"Matching target service {service.uuid()}")
-                printCharacteristics(device)
+
+def filterDevices(devices, targetUUID):
+    for device in devices:
+      filterDevice(device, targetUUID)
+
+
+def filterDevice(device, targetUUID):
+    if device.is_connectable():
+      print(f"Found: {device.identifier()} [{device.address()}]")
+      services = device.services()
+      for service in services:
+          if service.uuid() == targetUUID:
+              print(f"Matching target service {service.uuid()}")
+              if (device not in g.matchedDevices):
+                print(f"Device {device.identifier()} [{device.address()}] not in matched devices.")
                 g.matchedDevices.append(device)
-                connect_thread = threading.Thread(target=lambda: connectBT(device), daemon=True)
-                connect_thread.start()
+              else:
+                print(f"Device {device.identifier()} already stored.")
+          else:
+              print(f"Service not matching target")
+
+
+def handleBTConnections(frequecy = 3000):
+    pass
+    global BTAdapter, devicesChecked
+    if( not BTAdapter.scan_is_active() ): 
+      if( not devicesChecked ):
+        for device in g.matchedDevices:
+            if not device.is_connected():
+              connectBT(device)
+              # connect_thread = threading.Thread(target=lambda: connectBT(device), daemon=True)
+              # connect_thread.start()
             else:
-                print(f"Service {service.uuid()} not matching target {targetUUID}")
+              print(f"Device {device.identifier()} [{device.address()}] already connected.")
+        devicesChecked = True
+            
+
+def handleBTData(): # miss
+    pass
+
 
 def connectBT(device):
     print("Connecting to {}".format(device.address()))
-    device.set_callback_on_connected(lambda device: onConnectedDevice(device) )
-    device.set_callback_on_disconnected(lambda device: onDisconnectedDevice(device) )
-    device.connect()
-    # if(device.connect()):
-    #     printCharacteristics(device)
-    # else:
-    #     print("Failed to connect")
-    #     return
-    
+    device.set_callback_on_connected(lambda: onConnectedDevice(device) )
+    device.set_callback_on_disconnected(lambda: onDisconnectedDevice(device) )
+    try:
+      device.connect()
+    except:
+      print("Failed to connect to {}".format(device.address()))
+
+
+def onScanStart():
+    print("Scan started.")
+    g.isScanning = True
+
+def onScanStop():
+    global devicesChecked, BTAdapter
+    print("Scan complete.")
+    # filterDevices(BTAdapter.scan_get_results(), g.targetUUID)
+    devicesChecked = False
+    g.isScanning = False
+
+
 def onConnectedDevice(device):
     print(f"Connected to {device.address()}.")
+    printCharacteristics(device)
     g.matchedDevices.append(device)
+    
 
 def onDisconnectedDevice(device):
     print(f"Disconnected from {device.address()}.")
     g.matchedDevices.remove(device)
+
 
 def sendBT(sock, data):
     print("Sending {}".format(data))
@@ -84,14 +120,14 @@ def receiveBT(sock):
 def printCharacteristics( device ):
     services = device.services()
     #service_characteristic_pair = []
-    for service in services:
-        print(f"Service: {service.uuid()}")
-        for characteristic in service.characteristics():
-            print(f" - Char: {characteristic.uuid()}")
-            #service_characteristic_pair.append((service.uuid(), characteristic.uuid()))
-
-def handleBTConnections():
-    pass
-
-def handleBTData():
-    pass
+    if len(services) == 0:
+        print("No services found")
+    else:
+      for service in services:
+          print(f"Service: {service.uuid()}")
+          characts = service.characteristics()
+          if len(characts) == 0:
+              print("No characteristics found")
+          for characteristic in service.characteristics():
+              print(f" - Char: {characteristic.uuid()}")
+              #service_characteristic_pair.append((service.uuid(), characteristic.uuid()))
