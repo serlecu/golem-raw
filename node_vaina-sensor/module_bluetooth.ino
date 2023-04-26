18fb2a99fdb489574c9032f9da51c439e0408b9f
Sergio Lecuona <serlecu@hotmail.com>
2023-04-26 18:16:45 +0200

    Update module_bluetooth.ino
    - update characteristics
    - fixed buffer writing problems

--- node_vaina-sensor/module_bluetooth.ino
+++ node_vaina-sensor/module_bluetooth.ino
@@ -5,18 +5,16 @@ void setupBLE() {
   BLE.setLocalName( deviceName.c_str() );
   BLE.setAdvertisedService( customService );
   // add Custom Characteristics
-  customService.addCharacteristic(BMMagXChar);
-  customService.addCharacteristic(BMMagYChar);
-  customService.addCharacteristic(BMMagZChar);
-  customService.addCharacteristic(apdColorRChar);
-  customService.addCharacteristic(apdColorGChar);
-  customService.addCharacteristic(apdColorBChar);
-  customService.addCharacteristic(adpLuxChar);
+  customService.addCharacteristic(BMMagChar);
+  customService.addCharacteristic(BMAccelChar);
+  customService.addCharacteristic(BMGyroChar);
+  customService.addCharacteristic(apdLightChar);
+  customService.addCharacteristic(apdGestureChar);
   customService.addCharacteristic(adpProxChar);
   customService.addCharacteristic(hs3TempChar);
   customService.addCharacteristic(hs3HumChar);
   customService.addCharacteristic(lpsPressChar);
-  // customService.addCharacteristic(impulseResponseChar);
+  customService.addCharacteristic(impulseResponseChar);
   // add Service
   BLE.addService( customService );
   
@@ -27,7 +25,8 @@ void setupBLE() {
   BLE.advertise(); 
 
   // status prints
-  Serial.println("MAC: "+String(BLE.address()));
+  Serial.print("MAC: ");
+  Serial.println(String(BLE.address()));
   Serial.println("Waiting for connections...");
 }
 
@@ -77,78 +76,125 @@ void onConnected() {
 void blePeripheralConnectHandler( BLEDevice central ) {
   waitBleLed = true;
   inLedBlue(waitBleLed);
-  Serial.println("Connected to: "+String(central.address()));
+  // Serial.print("Connected to: ");
+  // Serial.println(String(central.address()));
 }
 
 void blePeripheralDisconnectHandler( BLEDevice central ) {
   waitBleLed = false;
   inLedBlue(waitBleLed);
-  Serial.println("Disconnected from: "+String(central.address()));
+  // Serial.print("Disconnected from: ");
+  // Serial.println(String(central.address()));
 }
 
 void publishValues() {
   // https://docs.arduino.cc/tutorials/nano-33-ble-sense/cheat-sheet
   // Serial.println("Start publishing ...");
-  // //Magnet
-  stringValue = String(VAINA_ID) + String(10) + String(valMagnetX);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  BMMagXChar.writeValue( byteArray, sizeof(byteArray) ); // float[-400,400]
-  stringValue = String(VAINA_ID) + String(11) + String(valMagnetY);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  BMMagYChar.writeValue( byteArray, sizeof(byteArray) ); // float[-400,400]
-  stringValue = String(VAINA_ID) + String(12) + String(valMagnetZ);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  BMMagZChar.writeValue( byteArray, sizeof(byteArray) ); // float[-400,400]
-  
-  // //Lux
-  stringValue = String(VAINA_ID) + String(20) + String(valLight);
-  stringValue.getBytes( byteArray, sizeof(byteArray) ); // int[0-255]
-  adpLuxChar.writeValue( byteArray, sizeof(byteArray) ); // float[-400,400]
 
-  // // //Color
-  // stringValue = String(VAINA_ID) + String(21) + String(valColorR) +","+String(valColorG)+","+String(valColorB);
-  stringValue = String(VAINA_ID) + String(22) + String(valColorR);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  apdColorRChar.writeValue( byteArray, sizeof(byteArray) ); // int[0-255]
-  stringValue = String(VAINA_ID) + String(22) + String(valColorG);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  apdColorGChar.writeValue( byteArray, sizeof(byteArray) ); // int[0-255]
-  stringValue = String(VAINA_ID) + String(23) + String(valColorB);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  apdColorBChar.writeValue( byteArray, sizeof(byteArray) ); // int[0-255]
-  
-  // // //Proximity
-  stringValue = String(VAINA_ID) + String(40) + String(valProximity);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  adpProxChar.writeValue( byteArray, sizeof(byteArray) ); // int[0-255]
-
-  // //Temperature
-  stringValue = String(VAINA_ID) + String(50) + String(valTemperature);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  hs3TempChar.writeValue( byteArray, sizeof(byteArray) ); // float[-40,120]
-  
-  //Humidity
-  stringValue = String(VAINA_ID) + String(60) + String(valHumidity);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  hs3HumChar.writeValue( byteArray, sizeof(byteArray) );
-  
-  //Presure
-  stringValue = String(VAINA_ID) + String(70) + String(valPressure);
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  lpsPressChar.writeValue( byteArray, sizeof(byteArray) );
-
-  // IR
-  String strArray = "";
-  for(int i = 0; i < FREQUENCY_BANDS; i++) {
-    strArray += String(resultsFFT[i], 2);
-    if(i < FREQUENCY_BANDS - 1) {
-      strArray += ",";
+  // Magnet
+  magnetValuesStr = "";
+  for(int i = 0; i < 3; i++) {
+    magnetValuesStr += String(magnetValues[i], 3);
+    if(i < 2) {
+      magnetValuesStr += ",";
     }
   }
-  stringValue = String(VAINA_ID) + String(80) + strArray;
-  stringValue.getBytes( byteArray, sizeof(byteArray) );
-  Serial.println("IR size: "+ String(sizeof(byteArray)) );
-  impulseResponseChar.writeValue(byteArray, sizeof(byteArray));
+  stringValue = String(VAINA_ID);
+  stringValue += String(10);
+  stringValue += magnetValuesStr; // float[3][-400,400]uT
+  stringValue.getBytes( magnetBytes, sizeof(magnetBytes) );
+  BMMagChar.writeValue( magnetBytes, sizeof(magnetBytes) );
+
+  // Accel
+  accelValuesStr = "";
+  for(int i = 0; i < 3; i++) {
+    accelValuesStr += String(accelValues[i], 3);
+    if(i < 2) {
+      accelValuesStr += ",";
+    }
+  }
+  stringValue = String(VAINA_ID);
+  stringValue += String(10);
+  stringValue += accelValuesStr; // float[3][-4, +4]g
+  stringValue.getBytes( accelBytes, sizeof(accelBytes) );
+  BMAccelChar.writeValue( accelBytes, sizeof(accelBytes) );
+
+  // Gyro
+  gyroValuesStr = "";
+  for(int i = 0; i < 3; i++) {
+    gyroValuesStr += String(gyroValues[i], 3);
+    if(i < 2) {
+      gyroValuesStr += ",";
+    }
+  }
+  stringValue = String(VAINA_ID);
+  stringValue += String(10);
+  stringValue += gyroValuesStr; // float[3][-2000, +2000]dps
+  stringValue.getBytes( gyroBytes, sizeof(gyroBytes) );
+  BMGyroChar.writeValue( gyroBytes, sizeof(gyroBytes) );
+
+  // Color & Light
+  lightValuesStr = "";
+  for(int i = 0; i < 4; i++) {
+    lightValuesStr += String(lightValues[i]);
+    if(i < 3) {
+      lightValuesStr += ",";
+    }
+  }
+  stringValue = String(VAINA_ID);
+  stringValue += String(20);
+  stringValue += lightValuesStr; // int[4][0-255]
+  stringValue.getBytes( lightBytes, sizeof(lightBytes) );
+  apdLightChar.writeValue( lightBytes, sizeof(lightBytes) );
+
+  // Gesture
+  stringValue = String(VAINA_ID);
+  stringValue += String(40);
+  stringValue += valGesture; // int[0-3]
+  stringValue.getBytes( gestBytes, sizeof(gestBytes) );
+  apdGestureChar.writeValue( gestBytes, sizeof(gestBytes) );
+  
+  // Proximity
+  stringValue = String(VAINA_ID);
+  stringValue += String(40);
+  stringValue += valProximity; // int[0-255]
+  stringValue.getBytes( proxBytes, sizeof(proxBytes) );
+  adpProxChar.writeValue( proxBytes, sizeof(proxBytes) );
+
+  // Temperature
+  stringValue = String(VAINA_ID);
+  stringValue += String(50);
+  stringValue += valTemperature; // float[-40,120]ºC
+  stringValue.getBytes( tempBytes, sizeof(tempBytes) );
+  hs3TempChar.writeValue( tempBytes, sizeof(tempBytes) ); 
+  
+  // Humidity
+  stringValue = String(VAINA_ID);
+  stringValue += String(60);
+  stringValue += valHumidity; // float [0-100]%
+  stringValue.getBytes( humBytes, sizeof(humBytes) );
+  hs3HumChar.writeValue( humBytes, sizeof(humBytes) );
+  
+  //Presure
+  stringValue = String(VAINA_ID);
+  stringValue += String(70);
+  stringValue += valPressure; // ???
+  stringValue.getBytes( pressBytes, sizeof(pressBytes) );
+  lpsPressChar.writeValue( pressBytes, sizeof(pressBytes) );
+
+  // IR
+  irValuesStr = "";
+  for(int i = 0; i < FREQUENCY_BANDS; i++) {
+    irValuesStr += String(resultsFFT[i], 2);
+    if(i < FREQUENCY_BANDS - 1) {
+      irValuesStr += ",";
+    }
+  }
+  stringValue = String(VAINA_ID);
+  stringValue += String(80);
+  stringValue += irValuesStr; // ???
+  stringValue.getBytes( irBytes, sizeof(irBytes) );
+  impulseResponseChar.writeValue(irBytes, sizeof(irBytes));
 
   justNotified = NOTIFICATION_BADGE_DECAY;
   //printValuesToSerial();
