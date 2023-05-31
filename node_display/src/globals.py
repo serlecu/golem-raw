d27c32508a2d31b27d0ee00abfc5dc6d1de77c59
Sergio Lecuona <serlecu@hotmail.com>
2023-05-31 08:59:39 +0200

    Update globals.py
    - add random gen macAddr list
    - add offlineMacList randomizator func

--- node_display/src/globals.py
+++ node_display/src/globals.py
@@ -2,12 +2,14 @@
 import pygame
 import uuid
 import time
+import random
+
 
 nodeID: str
 setupBleak: bool = False
 setupBless: bool = False
 setupPygame: bool = False
-lastLoopTime: float
+lastLoopTime: float = 0.0
 
 
 # Bluetooth
@@ -40,8 +42,37 @@ syncState = False
 # Pygame
 screen: pygame.Surface
 
+
+# Offline Mode
+offlineMode: bool = True
+offlineListLen: int
+offlineMacList: list = [
+    "SLAG_a9:dc:27 -> b3:e3:15:a9:dc:27",
+    "SLAG_b5:09:7e -> b3:e3:15:b5:09:7e",
+    "SLAG_ee:7c:71 -> b3:e3:15:ee:7c:71",
+    #"SLAG_de:92:da -> b3:e3:15:de:92:da",
+    "85:f5:75:ce:4a:e3",
+    "7e:4b:74:3f:1b:7d",
+    "a2:fd:1c:3f:97:9f",
+    "ed:ac:0d:81:05:be",
+    "9b:a5:2f:a2:9a:f9",
+    "b4:12:78:ea:01:d7",
+    "db:da:84:c9:95:66",
+    "b2:88:a1:4a:58:bc",
+    "b3:89:a7:94:39:5c",
+    "d5:17:69:c1:3c:15",
+    "b3:e3:15:e5:a7:65",
+    "eb:67:71:68:d8:a2",
+    "bf:a5:fb:0c:b0:47" ]#,
+    # "d3:44:01:6b:48:a5",
+    # "cf:d6:b4:d6:06:dd",
+    # "33:c9:74:00:c1:9d" ]
+
+
 def initGlobals():
-  global lastLoopTime, scannCrono, scannFrequency, isScanning, foundDevices, deviceInfo, nodeID, foundDevicesBleak
+  global lastLoopTime, scannCrono, scannFrequency, isScanning, foundDevices
+  global deviceInfo, nodeID, foundDevicesBleak, offlineMode, offlineListLen
+  
   lastLoopTime = time.time()
   nodeID = str(uuid.uuid1()).split("-")[1]
 
@@ -52,6 +83,18 @@ def initGlobals():
   foundDevicesBleak = []
   # ~ matchedClients = []
   deviceInfo = ""
+
+  if offlineMode:
+    offlineListLen = random.randint(10, 16)
+    print(f"Offline Mode: {offlineListLen} devices")
   
   print(f"Initialize Golem Node #{nodeID}")
+
+
+def shuffleOfflineList():
+  global offlineMacList, offlineListLen
+
+  offlineListLen = random.randint(10, 16)
+  random.shuffle(offlineMacList)
+
   
