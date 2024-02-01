82df51f56aea7de787bf8f593f81ee21824c71a7
Sergio Lecuona <serlecu@hotmail.com>
2024-02-01 19:32:24 +0100

    start making smoothMOvement

--- node_vaina-visualizer/src/graphics.py
+++ node_vaina-visualizer/src/graphics.py
@@ -33,6 +33,15 @@ slidesCrono: float = 0
 slidesUrn: list[int] = []
 slideNumber: int = 0
 
+magnet_data_displayed: list[float] = [0,0,0, 0,0,0]
+accel_data_displayed: list[float] = [0,0,0, 0,0,0]
+gyro_data_displayed: list[int] = [0,0,0, 0,0,0]
+lux_data_displayed: list[int] = [0,0,0, 0,0,0]
+proximity_data_displayed: list[int] = [0,0]
+atmos_data_displayed: list[float] = [0,0, 0,0, 0,0]
+ir_data_displayed: list[float]  = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0]
+
+
 # ====== SETUP ====== #
 
 def setupSimplePygame(surface):
@@ -99,7 +108,7 @@ def DrawLoop():
     pygame.mouse.set_pos( (0,2160) ) # reset mouse position
     updateDashboard(rendererB, tempSurfaceB)
     updateProyector(rendererA, tempSurfaceA)
-    
+
     # Rise onlyOnce flag
     if(onlyOnce):
         onlyOnce = False
@@ -127,11 +136,14 @@ def handlePygameInteraction(): # ON MAIN LOOP
 
 def updateDashboard(renderer, surface):
     global clock
+    global mag_data_displayed, accel_data_displayed, gyro_data_displayed
+    global light_data_displayed, prox_data_displayed, temp_data_displayed
+    global hum_data_displayed, press_data_displayed, ir_data_displayed
+
 
     # clean background
     surface.fill([0,0,0,255])
-    # draw graphics into surface
-    # test_text(surface, str(f"WIN_2 FPS: {round(clock.get_fps(),2)}") , (200,200), (255,255,255))
+
     # --- COLUMNA 1 --- #
     digitsList = []
     if g.noDataMode:
@@ -142,53 +154,57 @@ def updateDashboard(renderer, surface):
         digitsList = [int(i) for i in f"{int(g.scannCrono):02d}"]
     db.dib_numero( surface, num=digitsList[:2], pos=(150,200), size=(400,400), spacing=100 )
     
-    accel_data = []
+    accel_data: list[float] = []
     if g.noDataMode:
         accel_data = random.sample(range(0,800), 5)
-    elif g.serialMode:
+    else:
+    # elif g.serialMode:
         for value in g.accelVaina1:
                 mapValue = abs(value)  # form -1./+1. to 0./+1.
                 accel_data.append(clamp(mapValue, 0.0, 1.0) )
         for value in g.accelVaina2:
                 mapValue = abs(value)
                 accel_data.append(clamp(mapValue, 0.0, 1.0))
-    else:
-        for sensor in g.sensorDataList:
-            for value in sensor.getAccelerometer():
-                mapValue = abs(value)  # form -1./+1. to 0./+1.
-                accel_data.append(mapValue)
-    db.dib_sliders(surface, values=accel_data, pos=(200,900), size=(800,350), pointSize=10, vSpacing=50)
+    # else:
+    #     for sensor in g.sensorDataList:
+    #         for value in sensor.getAccelerometer():
+    #             mapValue = abs(value)  # form -1./+1. to 0./+1.
+    #             accel_data.append(mapValue)
+    db.dib_sliders(surface, values=smootherMovement(accel_data_displayed, accel_data), pos=(200,900), size=(800,350), pointSize=10, vSpacing=50)
     
     lux_data = []
     if g.noDataMode:
         lux_data = random.sample(range(0,255), 8)
-    elif g.serialMode:
+    else:
+    # elif g.serialMode:
         for value in g.lightVaina1:
                 lux_data.append(clamp(value *4, 0, 255))
         for value in g.lightVaina2:
                 lux_data.append(clamp(value *4, 0, 255))
-    else:
-        for sensor in g.sensorDataList:
-            for value in sensor.getLight():
-                lux_data.append(value)
-    db.dib_barras(surface, values=lux_data, pos=(200, 1500), maxWidth=800, stroke=20, spacing=20)
+    # else:
+    #     for sensor in g.sensorDataList:
+    #         for value in sensor.getLight():
+    #             lux_data.append(value)
+    db.dib_barras(surface, values=smootherMovement(lux_data_displayed, lux_data), pos=(200, 1500), maxWidth=800, stroke=20, spacing=20)
 
     # --- COLUMNA 2 --- #
     proximity_data = []
     if g.noDataMode:
         proximity_data = random.sample(range(0, 300), 3)
-    elif g.serialMode:
+    else:
+    # elif g.serialMode:
         proximity_data.append(g.proxVaina1)
         proximity_data.append(g.proxVaina2)
-    else:
-        for sensor in g.sensorDataList:
-            proximity_data.append(sensor.getProximity())
-    db.dib_arcos_sup(surface, values=proximity_data, pos=(1510, 200), size=(800,100))
+    # else:
+    #     for sensor in g.sensorDataList:
+    #         proximity_data.append(sensor.getProximity())
+    db.dib_arcos_sup(surface, values=smootherMovement(proximity_data_displayed, proximity_data), pos=(1510, 200), size=(800,100))
     
     rssi_data:list[float] = []
     if g.noDataMode:
         rssi_data = random.sample(range(60, 65), 2)
-    elif g.serialMode:
+    else:
+    # elif g.serialMode:
         if random.randint(0,10) == 0:
             rssi_data.append(round(random.randint(58, 70)/120, 1))
             rssi_data.append(round(random.randint(60, 80)/120, 1))
@@ -197,87 +213,89 @@ def updateDashboard(renderer, surface):
         else:
             rssi_data.append( g.rssiVaina1 )
             rssi_data.append( g.rssiVaina2 )
-    else:
-        for sensor in g.sensorDataList:
-            mapValue = abs(sensor.getRSSI()) / 120.0 # map to 0.-1.
-            rssi_data.append(mapValue)
+    # else:
+    #     for sensor in g.sensorDataList:
+    #         mapValue = abs(sensor.getRSSI()) / 120.0 # map to 0.-1.
+    #         rssi_data.append(mapValue)
     db.dibujoForma(surface, values=rssi_data, pos=(1920, 1000), size=300, amp=56, stroke=1)
     
-    ire_data = []
+    ir_data = []
     if g.noDataMode:
         for i in range(4):
-            ire_data.append([ random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1) ])
-    elif g.serialMode:
-        sensors = []
-        sensors.append(g.irVaina1)
-        sensors.append(g.irVaina2)
-        ire_data = sensors
+            ir_data.append([ random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1) ])
     else:
-        for sensor in g.sensorDataList:
-            ire_data.append(sensor.getImpulseResponse())
-    db.dib_circulorotos(surface, values=ire_data, thresholds=(10000,1000000), pos=(1510, 1600), size=(900,136), spacer=125)
+    # elif g.serialMode:
+        ir_data.append(g.irVaina1)
+        ir_data.append(g.irVaina2)
+    # else:
+    #     for sensor in g.sensorDataList:
+    #         ire_data.append(sensor.getImpulseResponse())
+    db.dib_circulorotos(surface, values=ir_data, thresholds=(10000,1000000), pos=(1510, 1600), size=(900,136), spacer=125)
 
     # --- COLUMNA 3 --- #
     gyro_data = []
     if g.noDataMode:
         gyro_data = random.sample(range(60,80), 4)
-    elif g.serialMode:
+    else:
+    # elif g.serialMode:
         for value in g.gyroVaina1:
                 mapValue = (value+0.5)*200
                 gyro_data.append(clamp(mapValue, 0, 100))
         for value in g.gyroVaina2:
                 mapValue = (value+0.5)*200
                 gyro_data.append(clamp(mapValue, 0, 100))
-    else:
-        for sensor in g.sensorDataList:
-            for value in sensor.getGyroscope():
-                mapValue = (value+0.5)*100 # map -0.5-+0.5 to 0-100
-                gyro_data.append(mapValue)
-    db.dib_lineapuntos(surface, values=gyro_data, pos=(2700, 200), size=(800,400) )
+    # else:
+    #     for sensor in g.sensorDataList:
+    #         for value in sensor.getGyroscope():
+    #             mapValue = (value+0.5)*100 # map -0.5-+0.5 to 0-100
+    #             gyro_data.append(mapValue)
+    db.dib_lineapuntos(surface, values=smootherMovement(gyro_data_displayed, gyro_data), pos=(2700, 200), size=(800,400) )
 
     magnet_data = []
     if g.noDataMode:
         magnet_data.append( [ float(random.randint(0,100)/100.0), float(random.randint(0,100)/100.0), float(random.randint(0,100)/100.0) ] )
         magnet_data.append( [ float(random.randint(0,100)/100.0), float(random.randint(0,100)/100.0), float(random.randint(0,100)/100.0) ] )
-    elif g.serialMode:
-        vaina1 = []
+    else:
+    # elif g.serialMode:
+        vaina1: list[int] = []
         for value in g.magVaina1:
             mapValue = float((value+100.0)/200)
             vaina1.append(mapValue)
         magnet_data.append(vaina1)
 
-        vaina2 = []
+        vaina2: list[int] = []
         for value in g.magVaina2:
             mapValue = float((value+100.0)/200.0) # map -100./+100 to 0./100
             vaina2.append(mapValue)
         magnet_data.append(vaina2)
-        db.dib_particulas(surface, values=magnet_data, pos=(2700, 800), size=(900, 400), scale=(0.9,0.9), stroke=1)
+    db.dib_particulas(surface, values=smootherMovement(magnet_data_displayed, magnet_data), pos=(2700, 800), size=(900, 400), scale=(0.9,0.9), stroke=1)
 
-    else:
-        for sensor in g.sensorDataList:
-            mapData:list = []
-            for value in sensor.getMagnetometer():
-                mapValue = float((value+100.0)/200) # map -100./+100 to 0./100
-                mapData.append(mapValue)
-            magnet_data.append(mapData)
-        db.dib_particulas(surface, values=magnet_data, pos=(2700, 800), size=(900, 400), scale=(1.0,1.0), stroke=1)
+    # else:
+    #     for sensor in g.sensorDataList:
+    #         mapData:list = []
+    #         for value in sensor.getMagnetometer():
+    #             mapValue = float((value+100.0)/200) # map -100./+100 to 0./100
+    #             mapData.append(mapValue)
+    #         magnet_data.append(mapData)
+    #     db.dib_particulas(surface, values=magnet_data, pos=(2700, 800), size=(900, 400), scale=(1.0,1.0), stroke=1)
 
     atmos_data = []
     if g.noDataMode:
         atmos_data = random.sample(range(0, 255), 10)
-    elif g.serialMode:
+    else:
+    # elif g.serialMode:
         atmos_data.append(g.tempVaina1)
         atmos_data.append(g.humVaina1)
         atmos_data.append(g.pressVaina1)
         atmos_data.append(g.tempVaina2)
         atmos_data.append(g.humVaina2)
         atmos_data.append(g.pressVaina2)
-    else:
-        for sensor in g.sensorDataList:
-            atmos_data.append(sensor.getTemp())
-            atmos_data.append(sensor.getHumidity())
-            atmos_data.append(sensor.getPressure())
-    db.dib_diagrama(surface, values=atmos_data, pos=(2900, 1400), size=(600, 400), radius=70, stroke=1)
+    # else:
+    #     for sensor in g.sensorDataList:
+    #         atmos_data.append(sensor.getTemp())
+    #         atmos_data.append(sensor.getHumidity())
+    #         atmos_data.append(sensor.getPressure())
+    db.dib_diagrama(surface, values=smootherMovement(atmos_data_displayed, atmos_data), pos=(2900, 1400), size=(600, 400), radius=70, stroke=1)
 
     # create texture from surface and render it to its window
     texture = Texture.from_surface(renderer, surface)
@@ -294,8 +312,6 @@ def updateProyector(renderer, surface):
 
     # clean background
     surface.fill([0,0,0,255])
-    # draw graphics into surface
-    # test_text(surface, str(f"WIN_1 FPS: {round(clock.get_fps(),2)}") , (200,200), (255,255,255))
 
     # Random pick a img number to change
     if (slidesCrono >= slidesFreq):
@@ -307,6 +323,7 @@ def updateProyector(renderer, surface):
             slidesUrn = []
         slidesCrono = 0
     db.dib_imag_estructura(surface, values=slideNumber, pos=(surface.get_width(),0), size=(600,600), spacing=140, mode=2)
+
     if g.serialMode:
         sensors = []
         vaina1:list[str] = []
@@ -355,6 +372,24 @@ def updateProyector(renderer, surface):
 
 def draw_background(surface, color):
     surface.fill(color)
+
+def smootherMovement(current, target):
+    if len(current) != len(target):
+        return print("ERROR: smootherMovement() - current and target must be the same size")
+    else:
+        if len(target) > 1:
+            for i in range(len(target)):
+                if current[i] < target[i]:
+                    current[i] += 2
+                elif current[i] > target[i]:
+                    current[i] -= 2
+        else:
+            if current < target:
+                current += 2
+            elif current > target:
+                current -= 2
+        
+        return current
 
 # testing animation
 def test_ellipse(surface, color, radius, angle):
